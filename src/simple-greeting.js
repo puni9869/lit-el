7ac898ec29857664accfceaee90e759620877e98
puni9869 <punitinani1@hotmail.com>
2022-02-14 13:49:28 +0530

    Event

--- src/simple-greeting.js
+++ src/simple-greeting.js
@@ -10,16 +10,38 @@ export class SimpleGreeting extends LitElement {
   static get properties() {
     return {
       name: { type: String },
+      data: {
+        type: String,
+        hasChanged: (value, oldValue) => {
+          console.log("has changed", oldValue, value);
+          return oldValue === value;
+        },
+      },
     };
   }
 
-  constructor() {
-    super();
+  connectedCallback() {
+    super.connectedCallback();
     this.name = '';
+    this.data = "";
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+
+  keyup(evt) {
+    if (evt.target.value) {
+      return false;
+    }
+    console.log(evt.target.value);
   }
 
   render() {
-    return html`<p>Hello ${this.name}!</p>`;
+    return html`
+      <p>Hello ${this.data}!</p> 
+      <input @keyup="${this.keyup}" type="text" .value="${this.data}">
+    `;
   }
 }
 customElements.define("hello-world", SimpleGreeting);
